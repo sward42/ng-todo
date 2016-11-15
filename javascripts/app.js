f9923f85b6ff303f74650449ac4b1713a439d7f4
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-11-14 21:32:15 -0600

    basic setup

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,47 @@
 "use strict";
 
-console.log("inside app.js")
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", function($scope){
+	$scope.navItems = [{name:"Logout"}, {name:"All Items"}, {name:"New Item"}];
+});
 
 
+app.controller("TodoCtrl", function ($scope) {
+	
+	$scope.welcome = "hello";
+	$scope.showListView = true;
+	$scope.items = [
+		{
+			id: 0,
+			task: "mow the lawn",
+			isCompleted: true,
+			assignedTo:"Zoe"
+		},
+		{
+			id: 1,
+			task: "grade quizzes",
+			isCompleted: false,
+			assignedTo:"William"
+		},
+		{
+			id: 2,
+			task: "take a nap",
+			isCompleted: false,
+			assignedTo:"Zoe"
+		}
+	];
+
+	$scope.allItems = function(){
+		console.log("you clicked all items" );
+		$scope.showListView = true;
+	}
+
+	$scope.newItem = function(){
+		console.log("you clicked new item" );
+		$scope.showListView = false;
+	}
+
+
+
+});
