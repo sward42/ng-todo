21a4cdbaee8f283bc822899553f91a88a91ab7f4
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-11-15 19:33:26 -0600

    add functionality

--- javascripts/app.js
+++ javascripts/app.js
@@ -11,6 +11,7 @@ app.controller("TodoCtrl", function ($scope) {
 	
 	$scope.welcome = "hello";
 	$scope.showListView = true;
+	$scope.newTask = {};
 	$scope.items = [
 		{
 			id: 0,
@@ -35,13 +36,20 @@ app.controller("TodoCtrl", function ($scope) {
 	$scope.allItems = function(){
 		console.log("you clicked all items" );
 		$scope.showListView = true;
-	}
+	};
 
 	$scope.newItem = function(){
 		console.log("you clicked new item" );
 		$scope.showListView = false;
-	}
+	};
 
-
+	$scope.addNewItem = function(){
+		$scope.newTask.isCompleted = false;
+		$scope.newTask.id = $scope.items.length;
+		console.log("newTask in function", $scope.newTask);
+		$scope.items.push($scope.newTask);
+		$scope.newTask = {};
+		$scope.showListView = true;
+	};
 
 });
