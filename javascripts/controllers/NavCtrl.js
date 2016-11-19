9f70d0433de44ea57f02a5c6dc3fbd7f90cc55b6
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-11-19 11:38:17 -0600

    added routing

--- javascripts/controllers/NavCtrl.js
+++ javascripts/controllers/NavCtrl.js
@@ -1,5 +1,18 @@
 "use strict";
 
 app.controller("NavCtrl", function($scope){
-	$scope.navItems = [{name:"Logout"}, {name:"All Items"}, {name:"New Item"}];
+	$scope.navItems = [
+	{
+		name:"Logout", 
+		url:"#/Logout"
+	}, 
+	{
+		name:"All Items", 
+		url:"#/items/list"
+	}, 
+	{
+		name:"New Item", 
+		url:"#/items/new"
+	}
+	];
 });
