52598a08033988904c614f62893090c80161d0eb
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-11-19 09:52:10 -0600

    day 2

--- javascripts/controllers/TodoCtrl.js
+++ javascripts/controllers/TodoCtrl.js
@@ -7,9 +7,13 @@ app.controller("TodoCtrl", function ($scope, ItemFactory) {
 	$scope.newTask = {};
 	$scope.items = [];
 
-	ItemFactory.getItemList().then(function(fbItems){
-		$scope.items = fbItems;	
-	});
+	let getItems = function(){
+		ItemFactory.getItemList().then(function(fbItems){
+			$scope.items = fbItems;	
+		});
+	};
+
+	getItems();
 
 	$scope.allItems = function(){
 		console.log("you clicked all items" );
@@ -23,11 +27,19 @@ app.controller("TodoCtrl", function ($scope, ItemFactory) {
 
 	$scope.addNewItem = function(){
 		$scope.newTask.isCompleted = false;
-		$scope.newTask.id = $scope.items.length;
-		console.log("newTask in function", $scope.newTask);
-		$scope.items.push($scope.newTask);
-		$scope.newTask = {};
-		$scope.showListView = true;
+		ItemFactory.postNewItem($scope.newTask).then(function(itemId){
+			getItems();
+			$scope.newTask = {};
+			$scope.showListView = true;
+
+		});
+	};
+
+	$scope.deleteItem = function(itemId){
+		console.log("you deleted me", itemId);
+		ItemFactory.deleteItem(itemId).then(function(response){
+			getItems();
+		});
 	};
 
 });
