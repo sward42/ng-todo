84fbe644d1ff7210fa0ee06daaf3ef2c86ffaea5
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-11-22 18:20:03 -0600

    auth controllers

---
+++ javascripts/controllers/AuthCtrl.js
@@ -0,0 +1,44 @@
+"use strict";
+
+app.controller("AuthCtrl", function ($scope, $location, $rootScope, AuthFactory, UserFactory) {
+
+	$scope.loginContainer = true;
+	$scope.registerContainer = false;
+
+	let logMeIn = function(loginStuff){
+			AuthFactory.authenticate(loginStuff).then(function(didLogin){
+			console.log("didLogin", didLogin);
+			return UserFactory.getUser(didLogin.uid);
+		}).then(function(userCreds){
+			$rootScope.user = userCreds;
+			$scope.login = {};
+			$scope.register = {};
+			$location.url("/items/list");
+		});
+	};
+
+	$scope.setLoginContainer = function(){
+		$scope.loginContainer = true;
+		$scope.registerContainer = false;
+
+	};
+
+	$scope.setRegisterContainer = function(){
+		$scope.loginContainer = false;
+		$scope.registerContainer = true;
+	};
+
+	$scope.registerUser = function(registerNewUser){
+		AuthFactory.registerWithEmail(registerNewUser).then(function(didRegister){
+			registerNewUser.uid = didRegister.uid;
+			return UserFactory.addUser(registerNewUser);
+		}).then(function(registerComplete){
+			logMeIn(registerNewUser);
+		});
+	};
+
+	$scope.loginUser = function(loginNewUser){
+		logMeIn(loginNewUser);
+	};
+
+});
